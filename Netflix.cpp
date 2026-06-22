c857ffca78b45611c353da1ea00b587349939f3c
YokeXuan3 <5005alicialim@gmail.com>
2026-06-22 14:09:37 +0800

    Welcome Screen

---
+++ Netflix.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+void titleColor(int color);      
+void welcomeScreen();
+
+void welcomeScreen()
+{
+    system("cls");
+
+    cout << "==========================================================\n";
+    titleColor(12);
+	cout << "  _   _ ______ _______ ______ _      _____ __   __\n";
+	cout << " | \\ | |  ____|__   __|  ____| |    |_   _|\\ \\ / /\n";
+	cout << " |  \\| | |__     | |  | |__  | |      | |   \\ V / \n";
+	cout << " | . ` |  __|    | |  |  __| | |      | |    > <  \n";
+	cout << " | |\\  | |____   | |  | |    | |____ _| |_  / . \\ \n";
+	cout << " |_| \\_|______|  |_|  |_|    |______|_____||_/ \\_\\\n";
+	cout << "\n";
+	titleColor(7);
+	cout << "                  BY REED HASTINGS\n";
+	cout << "===========================================================\n\n";
+
+    cout << " Welcome To Netflix!\n\n";
+
+    cout << " Explore the popular movies.\n";
+	cout << " Enjoy browsing!\n\n";
+
+    cout << "--------------------------------------------------------------\n\n";
+
+    cout << " Press ENTER to continue...";
+    cin.get();
+}
+
+void titleColor(int color)
+{
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
